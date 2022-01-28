38f0d32bfe023255e5ccabc2fb65514315361b6a
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-28 18:05:35 -0300

    Adição do carregaento de todas as notícias

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsModel } from 'src/app/core/models/news.model';
+import { NewsService } from 'src/app/core/service/news.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  newsList!: NewsModel[];
+  constructor(private service : NewsService) { }
+  
   ngOnInit(): void {
+    this.service.getNewsPreview().subscribe(data => {     
+      this.newsList = data;
+      console.log(this.newsList);
+    });
   }
+  
 
 }

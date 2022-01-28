88a1f3a21a79faaea9273d88bee71dbe8e4897ec
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-28 14:06:22 -0300

    Ajuste nas pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './views/home/home.component';
-import { CategoryListComponent } from './components/category-list/category-list.component';
+import { CategoryListComponent } from './core/components/category-list/category-list.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { CardComponent } from './components/card/card.component';
+import { SearchBarComponent } from './core/components/search-bar/search-bar.component';
+import { CardComponent } from './core/components/card/card.component';
 
 @NgModule({
   declarations: [

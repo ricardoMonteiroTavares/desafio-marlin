f129b9eea9116f58da80e801b7eaa2bcebf55b47
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-29 14:38:33 -0300

    Integração do router com o full-news

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullNewsComponent } from './views/full-news/full-news.component';
 import { HomeComponent } from './views/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },  
+  {
+    path: 'news/:id',
+    component: FullNewsComponent
   }
 ];
 
--- src/app/views/full-news/full-news.component.ts
+++ src/app/views/full-news/full-news.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NewsModel } from 'src/app/core/models/news.model';
+import { NewsService } from 'src/app/core/service/news.service';
 
 @Component({
   selector: 'app-full-news',
@@ -11,9 +13,20 @@ export class FullNewsComponent implements OnInit {
   @Input()
   news!: NewsModel;
   
-  constructor() { }
+  constructor(public router:Router, private service : NewsService) { }
 
   ngOnInit(): void {
+    let list = this.router.url.split('/');
+    let id = list[list.length - 1];
+    this.service.getNews(id).subscribe(data => {     
+      this.news = data;
+      console.log(this.news);
+    });
+    console.log(id);
+  }
+
+  getDate(){
+    return new Date(Date.parse(this.news.createdAt)).toLocaleDateString();
   }
 
 }

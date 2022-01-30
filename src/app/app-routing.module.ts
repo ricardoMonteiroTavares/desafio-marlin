d5d0e362f2429c53cb3e2bd9ae9199195c156ca7
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-30 12:16:56 -0300

    Adição de um routing para criar uma nova notícia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateNewsComponent } from './views/create-news/create-news.component';
 import { FullNewsComponent } from './views/full-news/full-news.component';
 import { HomeComponent } from './views/home/home.component';
 
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'news/:id',
     component: FullNewsComponent
+  },
+  {
+    path: 'create-news',
+    component: CreateNewsComponent
   }
 ];
 

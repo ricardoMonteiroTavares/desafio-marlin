69a12840993c646dc417d3c913273d285a9ae52a
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-31 13:39:13 -0300

    Correção de bug: Ajuste da quantidade de colunas em relação a quantidade de notícias

--- src/app/core/components/grid/grid.component.ts
+++ src/app/core/components/grid/grid.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { NewsModel } from '../../models/news.model';
 
 @Component({
@@ -6,26 +6,37 @@ import { NewsModel } from '../../models/news.model';
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.sass']
 })
-export class GridComponent implements AfterViewInit, OnInit {
+export class GridComponent implements AfterViewInit, OnInit, OnChanges {
 
   @ViewChild('grid') 
   gridElement!: ElementRef<HTMLDivElement>;
 
   @Input() newsList!: NewsModel[];
-
+  private newsLength!: number;
   private _gutterSize: number = 20;
   colsNumber!: number;
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(typeof changes["newsList"]["currentValue"] == 'undefined'){
+      this.newsLength = 1;
+    }else{
+      this.newsLength = changes["newsList"]["currentValue"].length;
+    }    
+  }
   ngOnInit(): void {
-    this.colsNumber = 1;
+    this.colsNumber = 1;    
+    this.newsLength = 1;
   }
 
   ngAfterViewInit(): void {
-    this.colsNumber = this.calculateCols(this.gridElement.nativeElement.offsetWidth);    
+    let result = this.calculateCols(this.gridElement.nativeElement.offsetWidth);     
+    this.colsNumber = (this.newsLength >= result)? result : this.newsLength;
   }
 
   handleSize(event: any): void {      
+    event.preventDefault();
     this.colsNumber = this.calculateCols(this.gridElement.nativeElement.offsetWidth);    
   }
 

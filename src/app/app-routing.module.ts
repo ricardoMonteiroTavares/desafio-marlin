f26ae03e3abcded28e5bd4e9d5228a8e377e431d
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-31 13:55:22 -0300

    Correção de bug: Não realização de uma busca, após uma busca ter sido realizada anteriormente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,12 +20,13 @@ const routes: Routes = [
   },
   {
     path: 'search/:value',
-    component: SearchPageComponent
+    component: SearchPageComponent,    
+    runGuardsAndResolvers: "always",
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: "reload"})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/views/search-page/search-page.component.ts
+++ src/app/views/search-page/search-page.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { NewsModel } from 'src/app/core/models/news.model';
 import { NewsService } from 'src/app/core/service/news.service';
 
@@ -8,20 +8,37 @@ import { NewsService } from 'src/app/core/service/news.service';
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.sass']
 })
-export class SearchPageComponent implements OnInit {
+export class SearchPageComponent implements OnInit, OnDestroy {
 
   newsList: NewsModel[] = [];
   searchTerm!: string;
+  navigationSubscription!: any;
 
-  constructor(public router:Router, private service : NewsService) { }
+  constructor(public router:Router, private service : NewsService) { 
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {     
+      if (e instanceof NavigationEnd) {
+        this.initialiseInvites();
+      }
+    });
+  }
 
-  ngOnInit(): void {
+  initialiseInvites() {
     let list = this.router.url.split('/');
     this.searchTerm = list[list.length - 1];    
     this.service.getNewsPreview().subscribe(data => {  
       let result = data.filter(value => this.filterMethod(value,this.searchTerm));   
       this.newsList = result;      
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.navigationSubscription) {  
+       this.navigationSubscription.unsubscribe();
+    }
+  }
+
+  ngOnInit(): void {
+    this.initialiseInvites();
   }
 
   private filterMethod(value: NewsModel, searchTerm: string) : boolean {

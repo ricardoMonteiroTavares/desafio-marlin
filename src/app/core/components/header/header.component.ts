2d4b8bc2bd5c95bcddf6869a4ad8593c0e253791
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-31 12:34:24 -0300

    Melhoria na exibição do header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Input() searchTerm!: string;
+  @Input() itemsFound!: number;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
+  public get title() : string {
+    return (typeof this.searchTerm === 'undefined')? "The news" : "Exibindo resultado de busca por \"" + this.searchTerm + "\"";
+  }
+  
+  
+  public get subtitle() : string {    
+    return (typeof this.itemsFound == 'undefined') ? "As melhores notícias, você encontra aqui!" : "Encontramos um total de " + this.itemsFound + " resultados para a sua busca";
+  }
+  
 
 }
